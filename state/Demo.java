c9de3bac829db3c4a65345f6c3e62924306acfcc
SofiaPikuza <37892488+SofiaPikuza@users.noreply.github.com>
2018-06-06 23:35:35 +0300

    Player version 1

---
+++ state/Demo.java
@@ -0,0 +1,14 @@
+
+import ui.Player;
+import ui.UI;
+
+/**
+ * Демо-класс. Здесь всё сводится воедино.
+ */
+public class Demo {
+    public static void main(String[] args) {
+        Player player = new Player();
+        UI ui = new UI(player);
+        ui.init();
+    }
+}
